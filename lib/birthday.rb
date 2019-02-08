ac2ebf96202a4257125df40129af18f3a159704b
Jacqi E <jackie.entz@gmail.com>
2019-02-08 09:03:56 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,5 +1,8 @@
-birthday_kids = {"Timmy" => 9, "Sarah" => 6, "Amanda" => 27}
-
+#birthday_kids = {
+ # "Timmy" => 9, 
+#   "Sarah" => 6, 
+ #  "Amanda" => 27
+ #}
 
 def happy_birthday(birthday_kids)
   
@@ -10,4 +13,4 @@ def happy_birthday(birthday_kids)
   end
 end
 end
-
+happy_birthday
